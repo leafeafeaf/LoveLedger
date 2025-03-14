2038a73395dff50cd44a9599139da205ee23039f
상승규 <grandsword25@naver.com>
2025-03-14 15:12:47 +0900

    #1 - feat : 엔터티 예시

--- backend/src/main/java/com/ssafy/loveledger/domain/user/domain/User.java
+++ backend/src/main/java/com/ssafy/loveledger/domain/user/domain/User.java
@@ -1,5 +1,56 @@
 package com.ssafy.loveledger.domain.user.domain;
 
-public class User {
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "User")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private Boolean gender;
+    private Date birthDate;
+    private String name;
+
+    @Column(name = "couple_id", nullable = false)
+    private Integer coupleId;
+
+//    @OneToOne(mappedBy = "owner", fetch = FetchType.LAZY)
+//    private Library library;
+//
+//    @OneToOne(mappedBy = "owner", fetch = FetchType.LAZY)
+//    private Account account;
+
+//    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+//    @BatchSize(size = 10)
+//    private List<DailyStatistics> dailyStatistics;
+//
+//    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+//    @BatchSize(size = 10)
+//    private List<MonthlyStatisticsByCategory> monthlyStatisticsByCategories;
+//
+//    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+//    @BatchSize(size = 10)
+//    private List<WeeklyStatisticsByCategory> weeklyStatisticsByCategories;
+//
+//    @OneToOne(mappedBy = "owner", fetch = FetchType.LAZY)
+//    private Goal goals;
 }
