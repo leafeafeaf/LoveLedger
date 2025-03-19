1f77ae8aa1bd74be452be6402ac586c2cd60a713
상승규 <grandsword25@naver.com>
2025-03-20 00:20:49 +0900

    #8 - feat : 계좌내역 엔터티 작성

--- backend/src/main/java/com/ssafy/loveledger/domain/history/domain/History.java
+++ backend/src/main/java/com/ssafy/loveledger/domain/history/domain/History.java
@@ -3,22 +3,31 @@ package com.ssafy.loveledger.domain.history.domain;
 import com.ssafy.loveledger.domain.account.domain.Account;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
-import java.time.LocalDateTime;
+import java.time.LocalDate;
 import java.time.LocalTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
 @Table
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter @Builder
 public class History {
 
     @Id
-    private Long transactionId;
+    private String transactionId;
 
     @ManyToOne
+    @JoinColumn(name = "account_id")
     private Account account;
 
-    private LocalDateTime createdDate;
+    private LocalDate createdDate;
     private LocalTime createdTime;
 
     private int transactionType;
@@ -29,6 +38,17 @@ public class History {
     private Long AmountAfterTransaction;
     private String memo;
     private String transactionTarget;
-
     private String summary;
+
+    private int categoryId;
+
+    private boolean isDeleted = false;
+
+    public void delete() {
+        this.isDeleted = true;
+    }
+
+    public void updateTargetName(String targetName) {
+        this.transactionTarget = targetName;
+    }
 }
