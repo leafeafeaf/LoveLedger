a1b69500373eefb3f8743036f8264aceee83d209
강성운 <tjddns09150915@gmail.com>
2025-03-25 10:18:20 +0900

    Revert "Merge branch 'feature/frontend-add-elem' into 'master'"
    This reverts merge request !21

--- backend/src/main/java/com/ssafy/loveledger/domain/statistics/domain/repository/DailyStatisticsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.loveledger.domain.statistics.domain.repository;
-
-import com.ssafy.loveledger.domain.statistics.domain.DailyStatistics;
-import com.ssafy.loveledger.domain.statistics.domain.key.DayId;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface DailyStatisticsRepository extends JpaRepository<DailyStatistics, DayId> {
-    
-}
