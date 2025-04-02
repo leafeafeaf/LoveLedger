c2bc120205b5407866f604a27ee57de36a44a0ca
backseungmin <als991225@naver.com>
2025-04-02 15:26:59 +0900

    #40 - fix : AI 소설 프롬프트 테마별 수정

--- backend/src/main/java/com/ssafy/loveledger/domain/library/presentation/FictionController.java
+++ backend/src/main/java/com/ssafy/loveledger/domain/library/presentation/FictionController.java
@@ -9,7 +9,6 @@ import com.ssafy.loveledger.domain.library.presentation.dto.response.fiction.Fic
 import com.ssafy.loveledger.domain.library.presentation.dto.response.fiction.FictionDetailReadResponse;
 import com.ssafy.loveledger.domain.library.service.FictionService;
 import com.ssafy.loveledger.domain.user.domain.User;
-import com.ssafy.loveledger.domain.user.domain.repository.UserRepository;
 import com.ssafy.loveledger.global.util.UserUtil;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -25,7 +24,6 @@ public class FictionController {
 
     private final FictionService fictionService;
     private final UserUtil userUtil;
-    private final UserRepository userRepository;
 
     // 소설 내용 생성
     @PostMapping("/content")
